4b54a7a86ccc1f9fa8fdb82b57aba70f862311b4
Mariel Setton <marielsetton@Mariels-MacBook-Pro.local>
2025-04-13 13:09:04 -0400

    add instructions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,26 @@ export default function Home() {
   return (
     <div className="items-center justify-items-center h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>Prepare your Garage invoice:</h1>
+        <h1>Instructions for preparing your Garage invoice:</h1>
         <form className="flex flex-col gap-4 items-center">
+          <div>
+            <ol className="list-decimal">
+              <li>
+                Copy the url of the firetruck listing that you want an invoice
+                for.
+              </li>
+              <li>
+                Place the url in the text box below to generate your invoice.
+              </li>
+              <li>
+                After clicking submit, you can choose whether to send it to your
+                email or print it.
+              </li>
+            </ol>
+          </div>
           <input
-            className="font-black bg-gray-50 flex items-center shadow border border-gray-300 rounded-md overflow-hidden"
+            name="listing-url"
+            className="w-full font-black bg-gray-50 flex items-center shadow border border-gray-300 rounded-md overflow-hidden"
             type="text"
           ></input>
           <div>
