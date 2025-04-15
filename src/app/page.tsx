1ec090acbdbf8b273b28345ce4b785789bed04b1
Mariel Setton <marielsetton@mariels-mbp.mynetworksettings.com>
2025-04-14 20:48:44 -0400

    fix homepage spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import GarageForm from "./garage";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="flex justify-center w-full h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-8 row-start-2 items-center">
         <h1>Instructions for preparing your Garage invoice:</h1>
         <GarageForm />
       </main>
